492f355a2d851d9f9d2dbf5c91ef35b3f698029b
Donghyeon Kim <0916dhkim@gmail.com>
2020-11-14 20:32:02 +0900

    A2MS2

--- A2/MS2/contactHelpers.c
+++ A2/MS2/contactHelpers.c
@@ -1,60 +1,139 @@
-//==============================================
-// Name:           Full name here
-// Student Number: #########
-// Email:          userID@myseneca.ca
-// Section:        XXX
-// Date:           
-//==============================================
-// Assignment:     2
-// Milestone:      2
-//==============================================
+/* -------------------------------------------
+Name: Donghyeon Kim
+Student number: 151613197
+Email: dkim167@myseneca.ca
+Section: NSS
+Date: 2020/11/14
+----------------------------------------------
+Assignment: 2
+Milestone:  2
+---------------------------------------------- */
 
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-
-// This source file needs to "know about" the functions you prototyped
-// in the contact helper header file.
-// HINT: Put the header file name in double quotes so the compiler knows
-//       to look for it in the same directory/folder as this source file
-//       #include your contactHelpers header file on the next line:
-
+#include "contactHelpers.h"
 
 //--------------------------------
 // Function Definitions
 //--------------------------------
 
-// +-------------------------------------------------+
-// | ====== Assignment 2 | Milestone 2 =======       |
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-1 |
-// |        empty function definitions below and     |
-// |        complete the definitions as per the      |
-// |        Milestone-2 specifications               | 
-// |                                                 |
-// | - The clearKeyboard function is done for you    |
-// +-------------------------------------------------+
-
-
 // Clear the standard input buffer
-void clearKeyboard(void)
-{
-    while (getchar() != '\n')   ; // empty execution code block on purpose
+void clearKeyboard(void) {
+    while (getchar() != '\n'); // empty execution code block on purpose
 }
 
-// pause function definition goes here:
+void pause(void) {
+    printf("(Press Enter to continue)");
+    clearKeyboard();
+}
 
+int getInt(void) {
+    char nl = 'x';
+    int value = 0;
 
-// getInt function definition goes here:
+    while (nl != '\n') {
+        scanf("%d%c", &value, &nl);
+        if (nl != '\n') {
+            // Invalid input.
+            clearKeyboard();
+            printf("*** INVALID INTEGER *** <Please enter an integer>: ");
+        }
+    }
 
+    return value;
+}
 
-// getIntInRange function definition goes here:
+int getIntInRange(int min, int max) {
+    int value = 0;
+    int valid = 0;
+    while (!valid) {
+        value = getInt();
+        if (value >= min && value <= max) {
+            valid = 1;
+        } else {
+            // Invalid input.
+            printf("*** OUT OF RANGE *** <Enter a number between %d and %d>: ", min, max);
+        }
+    }
 
+    return value;
+}
 
-// yes function definition goes here:
+int yes(void) {
+    char nl = 0;
+    char value = 0;
 
+    while (nl != '\n') {
+        scanf("%c%c", &value, &nl);
+        if (nl != '\n') {
+            // Invalid input.
+            clearKeyboard();
+            printf("*** INVALID ENTRY *** <Only (Y)es or (N)o are acceptable>: ");
+        }
+    }
 
-// menu function definition goes here:
+    if (value == 'Y' || value == 'y') {
+        return 1;
+    }
+    return 0;
+}
 
+int menu(void) {
+    // Display menu.
+    printf("Contact Management System\n");
+    printf("-------------------------\n");
+    printf("1. Display contacts\n");
+    printf("2. Add a contact\n");
+    printf("3. Update a contact\n");
+    printf("4. Delete a contact\n");
+    printf("5. Search contacts by cell phone number\n");
+    printf("6. Sort contacts by cell phone number\n");
+    printf("0. Exit\n\n");
+    printf("Select an option:> ");
 
-// contactManagerSystem function definition goes here:
+    return getIntInRange(0, 6);
+}
+
+void contactManagerSystem(void) {
+    int shouldExit = 0;
+    int option = 0;
+
+    while (!shouldExit) {
+        option = menu();
+        printf("\n");
+        switch (option) {
+            case 1:
+                printf("<<< Feature 1 is unavailable >>>\n\n");
+                pause();
+                break;
+            case 2:
+                printf("<<< Feature 2 is unavailable >>>\n\n");
+                pause();
+                break;
+            case 3:
+                printf("<<< Feature 3 is unavailable >>>\n\n");
+                pause();
+                break;
+            case 4:
+                printf("<<< Feature 4 is unavailable >>>\n\n");
+                pause();
+                break;
+            case 5:
+                printf("<<< Feature 5 is unavailable >>>\n\n");
+                pause();
+                break;
+            case 6:
+                printf("<<< Feature 6 is unavailable >>>\n\n");
+                pause();
+                break;
+            default:
+                printf("Exit the program? (Y)es/(N)o: ");
+                shouldExit = yes();
+                break;
+        }
+        printf("\n");
+    }
+
+    printf("Contact Management System: terminated\n");
+}
--- A2/MS2/contactHelpers.h
+++ A2/MS2/contactHelpers.h
@@ -1,16 +1,43 @@
-//==============================================
-// Name:           Full name here
-// Student Number: #########
-// Email:          userID@myseneca.ca
-// Section:        XXX
-// Date:           
-//==============================================
-// Assignment:     2
-// Milestone:      2
-//==============================================
+/* -------------------------------------------
+Name: Donghyeon Kim
+Student number: 151613197
+Email: dkim167@myseneca.ca
+Section: NSS
+Date: 2020/11/14
+----------------------------------------------
+Assignment: 2
+Milestone:  2
+---------------------------------------------- */
 
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-1 |
-// |        contents below...                        |
-// +-------------------------------------------------+
+/*--------------------------------------------------------------------
+Description: This header file modularizes general helper functions to
+             help reduce redundant coding for common repetitive tasks. 
+---------------------------------------------------------------------*/
+
+//--------------------------------
+// Function Prototypes
+//--------------------------------
+
+// Clear the standard input buffer
+void clearKeyboard(void);
+
+// Display a pause message and wait for the user
+// to press the <ENTER> key.
+void pause(void);
+
+// Get a valid integer.
+int getInt(void);
+
+// Get a valid integer between min and max (inclusive).
+int getIntInRange(int min, int max);
+
+// Return 1 if the user enters 'Y' or 'y'.
+// Return 0 if the user enters 'N' or 'n'.
+int yes(void);
+
+// Display a menu and let the user select an option.
+int menu(void);
+
+// contactManagerSystem:
+void contactManagerSystem(void);
 
